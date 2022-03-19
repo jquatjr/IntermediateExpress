75859056c1d0337ec8289b546f654ed29ba40373
jquatjr <91287863+jquatjr@users.noreply.github.com>
2022-03-19 02:28:26 -0400

    routes/books updated

--- ExpressBookstore/routes/books.js
+++ ExpressBookstore/routes/books.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const Book = require("../models/book");
+/** Routes for books in bookstore. */
 
+const express = require("express");
 const router = new express.Router();
 
+const { validate } = require("jsonschema");
+const bookSchemaNew = require("../schemas/bookSchemaNew");
+const bookSchemaUpdate = require("../schemas/bookSchemaUpdate");
+
+const Book = require("../models/book");
 
 /** GET / => {books: [book, ...]}  */
 
@@ -10,18 +15,22 @@ router.get("/", async function (req, res, next) {
   try {
     const books = await Book.findAll(req.query);
     return res.json({ books });
-  } catch (err) {
+  }
+
+  catch (err) {
     return next(err);
   }
 });
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.findOne(req.params.id);
+    const book = await Book.findOne(req.params.isbn);
     return res.json({ book });
-  } catch (err) {
+  }
+
+  catch (err) {
     return next(err);
   }
 });
@@ -30,9 +39,18 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const validation = validate(req.body, bookSchemaNew);
+    if (!validation.valid) {
+      return next({
+        status: 400,
+        error: validation.errors.map(e => e.stack)
+      });
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
-  } catch (err) {
+  }
+
+  catch (err) {
     return next(err);
   }
 });
@@ -41,9 +59,24 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    if ("isbn" in req.body) {
+      return next({
+        status: 400,
+        message: "Not allowed"
+      });
+    }
+    const validation = validate(req.body, bookSchemaUpdate);
+    if (!validation.valid) {
+      return next({
+        status: 400,
+        errors: validation.errors.map(e => e.stack)
+      });
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
-  } catch (err) {
+  }
+
+  catch (err) {
     return next(err);
   }
 });
@@ -58,5 +91,6 @@ router.delete("/:isbn", async function (req, res, next) {
     return next(err);
   }
 });
+
 
 module.exports = router;
